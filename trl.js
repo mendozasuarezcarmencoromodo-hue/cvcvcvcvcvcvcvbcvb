93f235102f8bb68b7fad970d1bac9f704366a6bf
mendozasuarezcarmencoromodo-hue <mendozasuarezcarmencoromodo@gmail.com>
2025-12-11 14:05:17 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1448661307887976569/Z1HrPukaEHSICpyUdqx5xl6qdYeDC2nI5siEdsf1gf2iuSN-tyQaqVP0siIQXNRTgk6D'; // Reemplaza esto con tu Webhook real
